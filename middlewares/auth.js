3e9efbc2cfdf58bdaea89e8422ad35a20d194f5a
satyam <mishrasatyam9890@gmail.com>
2025-05-05 13:46:12 +0530

    JWT Added

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,11 @@
 const { getUser } = require("../services/auth");
 
 async function restrictToLoggedIn(req, res, next) {
-    const userUid = req.cookies?.uid;
+    const userUid = req.headers['authorization'];
+
     if (!userUid) return res.redirect("/login");
-    const user = getUser(userUid);
+    const token = userUid.split("Bearer ")[1];
+    const user = getUser(token);
 
     if (!user) return res.redirect("/login");
 
@@ -12,9 +14,10 @@ async function restrictToLoggedIn(req, res, next) {
 }
 
 async function checkAuth(req, res, next) {
-    const userUid = req.cookies?.uid;
+    const userUid = req.headers['authorization'];
+    const token = userUid.split("Bearer ")[1];
     
-    const user = getUser(userUid);
+    const user = getUser(token);
 
     req.user = user;
     next();
